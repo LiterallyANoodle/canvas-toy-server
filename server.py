61cec350fd4b4da960188ff85f3fbbaba6893781
Matthew Mahan <themaskedkid321@gmail.com>
2025-03-26 23:07:51 -0500

    http requests from javascript sites work

--- server.py
+++ server.py
@@ -5,18 +5,29 @@ from base64 import b64decode
 from io import BytesIO
 import datetime
 from pathlib import Path
+import json
 
-# server params
-hostName = "localhost"
-serverPort = 6969
+class ServerConfiguration:
 
-# image params
-max_height = 500
-max_width = 500
-allowed_image_type = "PNG"
-Image.MAX_IMAGE_PIXELS = (max_height * max_width) # change pillow setting to prevent zip bomb style attack
+    configuration = {}
+
+    def __init__(self) -> None:
+        try: 
+            config_file = open("configuration.json")
+            self.configuration = json.loads(config_file.read())
+            Image.MAX_IMAGE_PIXELS = (self.configuration['max_height'] * self.configuration['max_width']) # change pillow setting to prevent zip bomb style attack
+        except:
+            print("Error: Unable to load server configuration.")
+            exit()
 
 class CanvasToyServer(BaseHTTPRequestHandler):
+
+    def do_OPTIONS(self) -> None:
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "POST,OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.end_headers()
     
     def do_POST(self) -> None:
 
@@ -26,18 +37,33 @@ class CanvasToyServer(BaseHTTPRequestHandler):
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
 
+        response_body = ''
+
         # verify content 
-        img = self.validate_image(body)
+        try: 
+            img = self.validate_image(body)
+        except: 
+            self.send_post_response(400, "Invalid message body.\n")
+            return
 
         # save to file 
-        self.save_image(img, self.client_address[0])
+        try:
+            self.save_image(img, self.client_address[0])
+            response_body += "Successfully saved image! \n"
+        except:
+            response_body += "Failed to save image. \n"
 
         # send on discord webhook 
 
+        self.send_post_response(200, response_body)
+
+    def send_post_response(self, status, response_body):
         # write headers based on status
-        self.send_response(200)
+        self.send_response(status)
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Content-type", "text/html")
         self.end_headers()
+        self.wfile.write(bytes(response_body, 'utf8'))
 
     def validate_image(self, body) -> Image:
         
@@ -49,18 +75,14 @@ class CanvasToyServer(BaseHTTPRequestHandler):
             img_bytes = BytesIO(b64decode(body_suffix))
 
             # load in pillow and verify contents
-            try:
-                img = Image.open(img_bytes)
-                img.verify()
-                print("Valid image.")
-                return Image.open(img_bytes)
-            except:
-                print("Invalid image.")
+            img = Image.open(img_bytes)
+            img.verify()
+            print("Valid image.")
+            return Image.open(img_bytes)
 
         except:
-            print("Invalid body.")
-
-        raise ValueError
+            print(f"{datetime.datetime.now()} Invalid body from {self.client_address[0]}")
+            raise ValueError
     
     def save_image(self, img, sender_ip) -> None:
 
@@ -73,18 +95,19 @@ class CanvasToyServer(BaseHTTPRequestHandler):
 
         # make unique name and save
         timestamp = datetime.datetime.now()
-        fp = Path('./saved_images')
+        fp = Path(config['saved_images_path'])
         img.save((fp / f'{str(timestamp).replace(':', '.')} {sender_ip}.png'), 'PNG')
 
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), CanvasToyServer)
-    print(f"Server started http://{hostName}:{serverPort}")
+if __name__ == "__main__":
+    config = ServerConfiguration().configuration
+    web_server = HTTPServer((config['host_name'], config['server_port']), CanvasToyServer)
+    print(f"Server started http://{config['host_name']}:{config['server_port']}")
 
     try:
-        webServer.serve_forever()
+        web_server.serve_forever()
     except KeyboardInterrupt:
         print("Received interrupt command.")
         pass
 
-    webServer.server_close()
+    web_server.server_close()
     print("Server stopped.")
