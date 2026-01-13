3c88472aec18a5e219d3197c6254dfe657fe52b4
Matthew Mahan <themaskedkid321@gmail.com>
2026-01-12 22:39:13 -0600

    Database integration and time metadata

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from PIL import Image
+from PIL.PngImagePlugin import PngInfo
+import piexif
 from base64 import b64decode
-from base64 import b64encode
 from io import BytesIO
 import datetime
 from time import time
@@ -10,6 +11,7 @@ from pathlib import Path
 import json
 from uuid import uuid4
 import http.client
+import psycopg2
 
 class ServerConfiguration:
 
@@ -51,7 +53,7 @@ class CanvasToyServer(BaseHTTPRequestHandler):
         body = self.rfile.read(content_length)
 
         response_body = ''
-
+        
         # verify content 
         try: 
             img, img_base64 = self.validate_image(body)
@@ -59,19 +61,28 @@ class CanvasToyServer(BaseHTTPRequestHandler):
             self.send_post_response(400, "Invalid message body.\n")
             return
 
-        # set background to white
+        # set background to white and add time
         try:
-            img = self.modify_image(img)
+            img = self.modify_image(img, timestamp)
         except Exception as e:
             print(type(e))
+            print(e)
             response_body += "Failed to modify image. \n"
+
+        # add to database
+        try:
+            img_UUID = self.insert_db(timestamp, '127.0.0.1')
+        except Exception as e:
+            print(type(e))
+            print(e)
 
         # save to file 
         try:
-            self.save_image(img, img_base64, timestamp)
+            self.save_image(img, img_UUID)
             response_body += "Successfully saved image!\n"
         except Exception as e:
             print(type(e))
+            print(e)
             response_body += "Failed to save image.\n"
 
         # send on discord webhook 
@@ -110,13 +121,47 @@ class CanvasToyServer(BaseHTTPRequestHandler):
             print(f"{datetime.datetime.now()} Invalid body from {self.client_address[0]}")
             raise ValueError
     
-    def save_image(self, img, img_base64, timestamp) -> None:
+    def save_image(self, img, img_UUID) -> None:
 
         # make unique name and save
         fp = Path(config['saved_images_path'])
-        img.save((fp / f'{str(timestamp).replace(":", ".")} {img_base64[:16]}.png').resolve(), 'PNG')
+        img.save((fp / f'{img_UUID}.png').resolve(), format='PNG', exif=img.info['exif'], pnginfo=img.info['pnginfo'])
 
-    def modify_image(self, img) -> Image:
+    def insert_db(self, timestamp, ip_source) -> str: 
+        
+        uuid = uuid4()
+
+        try:
+            print(f"Connecting to database {config['db_name']} on {config['db_host_name']}")
+            conn = psycopg2.connect(
+                dbname=config['db_name'],
+                host=config['db_host_name'],
+                user=config['db_user'],
+                password=config['db_pwd']
+            )
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM "Image" ORDER BY "gallery_number" DESC LIMIT 1;')
+            last_gallery_row = cur.fetchone()
+            if last_gallery_row != None:
+                last_gallery_number = last_gallery_row[4]
+            else:
+                last_gallery_number = 0
+            cur.execute(f'INSERT INTO \"Image\" (timestamp, ip_source, img_filename, gallery_number) VALUES (\'{timestamp.strftime('%Y-%m-%d %H:%M:%S')}\', \'{ip_source}\', \'{uuid}\', {last_gallery_number + 1});')
+
+            cur.close()
+        
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.commit()
+                conn.close()
+                print('Database conneciton closed.')
+                return uuid
+            
+        raise Exception('This code should never run.')
+
+    def modify_image(self, img, timestamp) -> Image:
 
         # remove transparency 
         print(f"Image in mode {img.mode}")
@@ -125,6 +170,23 @@ class CanvasToyServer(BaseHTTPRequestHandler):
             background = Image.new('RGB', img.size, (255, 255, 255))
             background.paste(img, mask=alpha)
             img = background
+
+        # add date time metadata
+        exif_dict = {
+            "0th": {
+                piexif.ImageIFD.DateTime: timestamp.strftime('%Y:%m:%d %H:%M:%S')
+            },
+            "Exif": {
+                piexif.ExifIFD.DateTimeOriginal: timestamp.strftime('%Y:%m:%d %H:%M:%S'),
+                piexif.ExifIFD.DateTimeDigitized: timestamp.strftime('%Y:%m:%d %H:%M:%S'),
+            }
+        }
+        exif_bytes = piexif.dump(exif_dict)
+        metadata = PngInfo()
+        metadata.add_text('Creation Time', timestamp.strftime('%Y:%m:%d %H:%M:%S'))
+
+        img.info['exif'] = exif_bytes
+        img.info['pnginfo'] = metadata
 
         return img
 
